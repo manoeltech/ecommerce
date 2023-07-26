a1d86427fb4ceb3217d7b94675899b2d56ccc900
manoeltech <manoel.programador21@gmail.com>
2023-07-26 10:07:58 -0300

    Improve model coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,7 +1,12 @@
+require_relative 'validators/future_date_validator'
+
 class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: { case_sensitive: false }
-  validates :status, :due_date, presence: true
-  validates :discount_value, presence: true, numericality: { onlY_integer: true, greater_than: 0 }
+  validates :status, presence: true
+  validates :due_date, presence: true, future_date: true
+  validates :discount_value, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
   enum status: { active: 1, inactive: 2 }
 end
+
+ 
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Coupon, type: :model do
+  subject { build(:coupon) }
+
   it { is_expected.to validate_presence_of(:code) }
   it { is_expected.to validate_uniqueness_of(:code).case_insensitive }
 
@@ -11,4 +13,22 @@ RSpec.describe Coupon, type: :model do
 
   it { is_expected.to validate_presence_of(:status) }
   it { is_expected.to define_enum_for(:status).with_values({ active: 1, inactive: 2 })}
+
+  it "can't have past due_date" do
+    subject.due_date = 1.day.ago
+    subject.valid?
+    expect(subject.errors.key?(:due_date)).to be_truthy
+  end
+  
+  it "is invalid with current due_date" do
+    subject.due_date = Time.zone.now
+    subject.valid?
+    expect(subject.errors.key?(:due_date)).to be_truthy
+  end
+  
+  it "is valid with future date" do
+    subject.due_date = Time.zone.now + 1.hour
+    subject.valid?
+    expect(subject.errors.key?(:due_date)).to be_falsey
+  end
 end
