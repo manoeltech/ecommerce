6ccdcc83828cb8f8f0ec31ef60223ad41063fa86
manoeltech <manoel.programador21@gmail.com>
2023-07-27 08:00:38 -0300

    Include def create and private in category controller.

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -5,5 +5,20 @@ module Admin::V1
       @categories = Category.all
     end
     
+    def create
+      @category = Category.new
+      @category.attributes = category_params
+
+      @category.save!
+      render :show
+    rescue
+      render json: { errors: { fields: @category.errors.messages } }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  category_params 
+  return {} unless params.has_key?(:category)
+  params.require(:category).permit(:name)  
   end
  end
